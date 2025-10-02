6036dae711b66c3d59be599e83da8ad2f2fa1410
Sazid <sazid479@gmail.com>
2025-10-02 14:14:00 +0600

    All the files has been comitted

---
+++ Day1/Problem1 (Better Approach).py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        # 1, 2 , 4, 7, 11, 15
+        # target = 9
+        left = 0
+        right = len(numbers) - 1
+        while (left < right):
+            s = numbers[left] + numbers[right]
+            if s == target:
+                return [left + 1, right + 1]
+            elif s > target:
+                right -= 1
+            elif s < target:
+                left += 1
+        return None

e00ac037e6a44afdd3d238f16ced86bd17fe2380
Sazid <sazid479@gmail.com>
2025-10-11 00:21:23 +0600

    Day 8

--- Day 8/Problem 3.py
+++ Day 8/Problem 3.py
@@ -1,3 +1,7 @@
-import math
-x = math.log(8, 2)
-print(x)
+class Solution(object):
+    def maxDepth(self, root):
+        if root == None:
+            return 0
+        leftH = self.maxDepth(root.left)
+        rightH = self.maxDepth(root.right)
+        return (max(leftH, rightH) + 1)

1c94ce2d8ab8c73ddbe1b1cc68d3e523a02c1edd
Sazid <sazid479@gmail.com>
2025-10-25 21:54:50 +0600

    Done for today

---
+++ Day 11/Problem 3.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        result = []
+        def dfs(node, level):
+            if node is None:
+                return
+            if level == len(result):
+                result.append(node.val)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+        dfs(root, 0)
+        return result
